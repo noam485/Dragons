73d529730fefa4a7eb875235c5fd8ceca3753a8c
noamy <noam.yehezkel@gmail.com>
2023-06-24 16:58:12 +0300

    .

--- Main.java
+++ Main.java
@@ -3,7 +3,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		RealIsland realIsland = new RealIsland(4);
-		for (int i=0; i< 10; i++) {
+		for (int i=1; i< 10; i++) {
+			System.out.println("day " + i);
 			realIsland.updateLeavingDragonsExpectation();
 			realIsland.awareBlueEyedRealDragonsLeave();
 			realIsland.updatePossibleImaginedIslands();
